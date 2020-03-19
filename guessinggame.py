8b2fbf9a96cb09bb57f5e17730f97ec2371015ed
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-03-19 15:41:40 -0400

    while loop to keep guessing number untill matched

--- guessinggame.py
+++ guessinggame.py
@@ -2,19 +2,22 @@ import random
 
 highest = 10
 answer = random.randint(1, highest)
-
+guess = 0  # initialize to any number that doesn't equal the answer
 print("Please guess number between 1 and {}: ".format(highest))
-guess = int(input())
 
-if guess != answer:
-    if guess < answer:
-        print("Please guess higher")
-    else:   # guess must be greater than answer
-        print("Please guess lower")
+while guess != answer:
     guess = int(input())
+
     if guess == answer:
-        print("Well done, you guessed it")
+        print("Well done , you guessed it")
+        break
     else:
-        print("Sorry, you have not guessed correctly")
-else:
-    print("You got it first time")
+        if guess < answer:
+            print("Please guess higher")
+        else:
+            print("Please guess lower")
+      #  guess = int(input())
+      #  if guess == answer:
+      #      print("Well done, you guessed it")
+      #  else:
+      #      print("Sorry, you have not guessed correctly")
